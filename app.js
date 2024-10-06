13d1ffe2daeace67c2eabdd9bc08f224228ac889
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-06 08:18:36 +0500

    scope

---
+++ app.js
@@ -0,0 +1,39 @@
+// var x = 10; // Global Scope
+// function myFunction() {
+//     // x+=10;
+//     // x= x+20
+//   console.log(x); // Accessible here
+// }
+// myFunction(); // Output: 10
+
+
+// function myFunction() {
+//     var y = 5; // Local Scope
+//     console.log(y); // Accessible here
+// }
+
+// myFunction();
+// console.log(y); // Error: y is not defined
+
+
+// {
+//     let z = 15; // Block Scope
+//     const w = 20; // Block Scope
+//     console.log(z); // Accessible here
+//     console.log(w); // Accessible here
+// }
+
+// console.log(z); // Error: z is not defined
+// console.log(w); // Error: w is not defined
+
+
+
+// if (true) {
+//     var a = 1;   // Function Scope
+//     let b = 2;   // Block Scope
+//     const c = 3; // Block Scope
+// }
+
+// console.log(a); // Accessible: 1
+// console.log(b); // Error: b is not defined
+// console.log(c); // Error: c is not defined
